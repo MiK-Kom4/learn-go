9de6403420da7449ee4e5607136ab7cf103d9977
Mik-kom4 <androidmikiya9814@gmail.com>
2024-09-24 19:00:09 +0900

    3-9 他言語とだいたい一緒

--- lesson.go
+++ lesson.go
@@ -2,17 +2,22 @@ package main
 
 import "fmt"
 
-const Pi = 3.1415926
-
-const (
-	Username = "test_user"
-	Password = "test_pass"
-)
-
-// var big int = 9223372036854775807 + 1
-const big = 9223372036854775807 + 1
-
 func main() {
-	fmt.Println(Pi, Username, Password)
-	fmt.Println(big - 1)
+	//var (
+	//	u8  uint8     = 255
+	//	i8  int8      = 127
+	//	f32 float32   = 32.0
+	//	c64 complex64 = -5 + 12i
+	//)
+	//fmt.Println(u8, i8, f32, c64)
+	//fmt.Printf("%T %v\n", u8, u8)
+	fmt.Println("1+1=", 1+1)
+	fmt.Println("10 - 1 =", 10-1)
+	fmt.Println("10 / 2 =", 10/2)
+	fmt.Println("10 / 3 =", 10/3)
+	fmt.Println("10.0 / 3 =", 10.0/3)
+	fmt.Println("10.0 / 3 =", 10.0/3)
+	fmt.Println("10 / 3.0 =", 10/3.0)
+	fmt.Println("10 % 2 =", 10%2)
+	fmt.Println("10 % 3 =", 10%3)
 }
